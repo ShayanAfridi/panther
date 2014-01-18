309c2eac07210ec04e81dae6febdc8d5f5beb86b
Shayan <palmtreetutoring@gmail.com>
2014-01-18 18:38:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Panther::Application.config.secret_key_base = 'ebd1b747ae409dc67061d528fed6e9c159ccb6c57c936aa39eed1b972278349942dc43acf9ebb1036e5ff7e09db0fa31ace1ebfae7596a31cd9c551b88d7c03d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Panther::Application.config.session_store :cookie_store, key: '_panther_session'
